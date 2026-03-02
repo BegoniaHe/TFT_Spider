821c13c909138e1e7f00703d4afa86ad0aff949d
BegoniaHe <tianyihebegonia1005@gmail.com>
2026-03-02 12:50:00 +0100

    fix(config): update data directory paths for consistency

--- tft_spider/config.py
+++ tft_spider/config.py
@@ -4,9 +4,9 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-_DATA_DIR = os.path.join(ROOT_DIR, "tft_data")
+_DATA_DIR = os.path.join(ROOT_DIR, "data/tft_data")
 
 TFT_RAW_DATA_FILE = os.path.join(_DATA_DIR, "tft_raw_data.json")
 TFT_PROCESSED_DATA_FILE = os.path.join(_DATA_DIR, "tft_processed_data.json")
 TFT_PY_CLASS_FILE = os.path.join(_DATA_DIR, "TFTData.py")
-TFT_IMG_FILE = os.path.join(ROOT_DIR, "tft_images")
+TFT_IMG_FILE = os.path.join(_DATA_DIR, "tft_images")
